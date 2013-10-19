609daaa161d2af8ab6983c5a963135863feb43e3
Niima3 <southbird536@yahoo.co.jp>
2013-10-20 06:36:41 +0900

    first commit

---
+++ test.py
@@ -0,0 +1,18 @@
+'''
+Created on 2013/10/20
+
+@author: nima
+'''
+# -*- coding: utf-8 -*-
+# tsurukame.py
+
+
+from pylab import *
+
+x  = arange(-10.0, 10.0, 0.1)
+
+y = sin(x)
+
+plot(x,y)
+
+show()
